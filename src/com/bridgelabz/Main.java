e5223523c9395da0aa7552391c0a5eef91959ef3
sunnyok03 <sunnyok03@gmail.com>
2023-12-01 23:04:40 +0530

    UC3 validate email

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -7,9 +7,14 @@ public class Main {
         System.out.println(UC1.validateFirstName("Rahul")); // true
         System.out.println(UC1.validateFirstName("rahul")); // false
 
-        //As a User need to enter a valid First Name
+        //As a User need to enter a valid last Name
         UserRegistration UC2 = new UserRegistration();
-        System.out.println(UC2.validateFirstName("Kumar")); // true
-        System.out.println(UC2.validateFirstName("kumar")); // false
+        System.out.println(UC2.validateLastName("Kumar")); // true
+        System.out.println(UC2.validateLastName("kumar")); // false
+
+        //As a User need to enter a valid email
+        UserRegistration UC3 = new UserRegistration();
+        System.out.println(UC3.validateEmail("abc.xyz@bl.co.in")); // true
+        System.out.println(UC3.validateEmail("abz.rahul@bl.co.in")); // false
     }
 }
--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -22,11 +22,27 @@ public class UserRegistration {
     }
 
         /*
-    @desc: validate name as first name starts with Cap and has minimum 3 characters
+    @desc: validate name as last name starts with Cap and has minimum 3 characters
     @params: firstName as String
     @return: true if valid else false
      */
     public boolean validateLastName(String lastName){
         return validateFirstName(lastName);
+    }
+
+    /*
+    @desc: validate email.
+            E.g:- abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co)
+            and 2 optional (xyz & in) with
+            precise @ and . positions
+    @params: email as String
+    @return: true if valid else false
+     */
+    public boolean validateEmail(String email){
+        String nameRegex = "[a-zA-Z0-9]+([._%+-]+[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\\\.[a-zA-Z]{2,}){1,2}$";
+        Pattern pattern = Pattern.compile(nameRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 }
