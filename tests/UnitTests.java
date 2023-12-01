1e0bb23b8ec9fff57c8fd4679d30978c27221bb8
sunnyok03 <sunnyok03@gmail.com>
2023-12-02 00:13:39 +0530

    UC11 unit tests for multiple emails

--- tests/UnitTests.java
+++ tests/UnitTests.java
@@ -24,4 +24,37 @@ public class UnitTests {
         assertFalse(user.validatePhone("91 0101001"));
         assertFalse(user.validatePassword("abd@skdk"));
     }
+
+    @Test
+    public void emailsTest(){
+        UserRegistration user = new UserRegistration();
+        String[] validEmails = {"abc@yahoo.com",  //true
+                "abc-100@yahoo.com", // true
+                "abc.100@yahoo.com", //true
+                "abc111@abc.com", //true
+                "abc-100@abc.net", //true
+                "abc.100@abc.com.au",  //true
+                "abc@1.com", // true
+                "abc+100@gmail.com", // true
+                };
+        String[] invalidEmails = {"abc", // false
+                "abc@.com.my", //false
+                "abc123@gmail.a", //false
+                "abc123@.com", //false
+                "abc123@.com.com", //false
+                ".abc@abc.com", //false
+                "abc()*@gmail.com",//false
+                "abc@%*.com",//false
+                "abc..2002@gmail.com",//false
+                "abc.@gmail.com",//false
+                "abc@abc@gmail.com",//false
+                "abc@gmail.com.1a",//false
+                "abc@gmail.com.aa.au"};//false
+        for(String email : validEmails){
+            assertTrue(user.validateEmail(email));
+        }
+        for(String email : invalidEmails){
+            assertFalse(user.validateEmail(email));
+        }
+    }
 }
