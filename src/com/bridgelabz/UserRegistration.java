17216dc14ff857324d9f5f36404b38280be391e9
sunnyok03 <sunnyok03@gmail.com>
2023-12-01 23:20:32 +0530

    UC8 validate password rule4

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -65,7 +65,7 @@ public class UserRegistration {
  @return: true if valid else false
  */
     public boolean validatePassword(String password){
-        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d).{8,}$";
+        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$";
         Pattern pattern = Pattern.compile(passwordRegex);
         Matcher matcher = pattern.matcher(password);
 
